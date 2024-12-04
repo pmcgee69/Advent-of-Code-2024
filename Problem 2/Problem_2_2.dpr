78563241a1bc05cd99a67272447574d2f4c273cb
paul.mcgee.8@bigpond.com <paulmcgee1969@gmail.com>
2024-12-04 13:53:25 +0800

    Problem 2.2 - template

---
+++ Problem 2/Problem_2_2.dpr
@@ -0,0 +1,66 @@
+{$APPTYPE CONSOLE}
+program Problem_2_2;
+
+uses
+  System.SysUtils,
+  System.Math,
+  System.Generics.Collections,
+  UProblem2_Data in 'UProblem2_Data.pas',
+  U_Utils_Functional in '..\U_Utils_Functional.pas';
+
+{$region 'Challenge 2.2'}
+// Advent of Code 2024 - Problem 2 - Part 1
+// https://adventofcode.com/2024/day/1
+//
+// Now, the same rules apply as before, except
+// if removing a single level from an unsafe report would make it safe,
+// the report instead counts as safe.
+//
+// More of the above example's reports are now safe:
+//
+//  7 6 4 2 1: Safe without removing any level.
+//  1 2 7 8 9: Unsafe regardless of which level is removed.
+//  9 7 6 2 1: Unsafe regardless of which level is removed.
+//  1 3 2 4 5: Safe by removing the second level, 3.
+//  8 6 4 4 1: Safe by removing the third level, 4.
+//  1 3 6 7 9: Safe without removing any level.
+//
+// 4 reports are now safe.
+// Update your analysis by handling situations where
+// the Problem Dampener can remove a single level from unsafe reports.
+{$endregion}
+
+function monotone_nonconstant_bounded(a:TArray<integer>):boolean;
+begin
+
+   var deltas := UFP.Leave<integer>( UFP.Adj_Delta(a), 1 );
+
+   if abs(UFP.List_Reduce<integer>(deltas,max,minint)) > 3 then exit(false);      //  NO jump of zero, or greater than 3
+   if abs(UFP.List_Reduce<integer>(deltas,min,maxint)) = 0 then exit(false);      //
+
+   var diffs := UFP.Leave<integer>( UFP.Adj_Diff(a), 1 );
+
+   var min_diff := UFP.List_Reduce<integer>(diffs,min,maxint);
+   var max_diff := UFP.List_Reduce<integer>(diffs,max,minint);
+
+   if sign(min_diff) <> sign(max_diff) then exit(false)                           // ALL changes must be monotonic
+                                       else exit(true);
+end;
+
+
+begin
+   var k  := 0;
+   var sl := get_data;                                   // Pull data set entries one at a time,
+       repeat                                            // a bit like a coroutine / python generator function
+         var b := monotone_nonconstant_bounded(sl);      // or a database
+
+             //if not b then explore fuzzy options
+
+             if b then inc(k);
+             sl := get_data;
+       until (sl=nil);
+
+       writeln;
+       writeln('Number of safe level sets : ',k);        // 230
+       readln;
+end.
