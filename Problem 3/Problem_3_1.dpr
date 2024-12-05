bd9fa838040551479b472cb61d6d945ff0ccdd3b
paul.mcgee.8@bigpond.com <paulmcgee1969@gmail.com>
2024-12-05 21:42:06 +0800

    Problem 3.1
    Some inspiration taken from the idea of Parser Combinators.

---
+++ Problem 3/Problem_3_1.dpr
@@ -0,0 +1,67 @@
+﻿{$APPTYPE CONSOLE}
+program Problem_3_1;
+
+uses
+  System.SysUtils,
+  System.Classes,
+  U_Parsers in 'U_Parsers.pas',
+  System.Generics.Collections,
+  U_Utils_Functional in '..\U_Utils_Functional.pas';
+
+{$region 'Challenge 3.1'}
+// Advent of Code 2024 - Problem 3 - Part 1
+// https://adventofcode.com/2024/day/3
+//
+{$endregion}
+
+procedure bump(var st:TStringStream);
+begin
+       st.Position:=st.Position+1;
+end;
+
+begin
+   var total : int64 := 0;
+   var mult          := 0;
+
+   var stream := TStringStream.Create;
+       stream.LoadFromFile('..\..\problem 3 raw data.txt');
+
+   var mul, dig, com, par : TParseResult;
+   var i := 0;
+
+       repeat
+             mul := Parse_Mul(stream);
+             if mul.ok then begin
+
+                dig := Parse_Digit(mul);                            // position doesn't advance
+                if dig.ok then begin
+
+                    bump(stream);
+                    repeat
+                        dig := Parse_Digit(dig);                    // position doesn't advance
+                        com := Parse_Comma(dig);                    //
+                        if dig.ok or com.ok then bump(stream);
+                    until not dig.ok;
+
+                    if com.ok then begin
+                        dig := com;
+                        repeat
+                            dig := Parse_Digit(dig);                // position doesn't advance
+                            par := Parse_EndParen(dig);             //
+                            if dig.ok or par.ok then bump(stream);
+                        until not dig.ok;
+
+                        if par.ok then begin                        // par = '000,111'
+                           var s    := par.s.Split([',']);
+                               mult := s[0].ToInteger * s[1].ToInteger;
+                               inc(total, mult);
+                               inc(i);
+                        end;
+                    end;
+                end;
+             end;
+       until stream.position = stream.size;
+
+       writeln(total);           // 173785482
+       readln;
+end.
---
+++ Problem 3/U_Parsers.pas
@@ -0,0 +1,88 @@
+unit U_Parsers;
+
+interface
+uses System.SysUtils, System.Classes;
+
+type
+  TParseResult = record
+    ok  : boolean;
+    s   : string;
+    str : TStringStream;
+  end;
+
+  function Parse_Mul  (st:TStringstream):TParseResult;
+  function Parse_Digit({const [ref]} pr:TParseResult ):TParseResult;
+  function Parse_Comma(pr:TParseResult ):TParseResult;
+  function Parse_EndParen(pr:TParseResult ):TParseResult;
+
+implementation
+
+type
+  TDigits = set of '0'..'9';
+var
+  Digits : TDigits = ['0'..'9'];
+
+  function Parse_Mul(st:TStringstream):TParseResult;
+  begin
+          result.ok  := false;
+          result.s   := '';
+          result.str := st;
+      var next       := st.Position+1;
+
+          if st.ReadString(4) <> 'mul(' then
+             st.Position := next
+          else begin
+             result.ok := true;
+             result.s  := '';
+          end;
+  end;
+
+  function Parse_Digit({const [ref]} pr:TParseResult):TParseResult;
+  begin
+        result.ok  := false;
+        result.s   := pr.s;
+        result.str := pr.str;
+    var same       := pr.str.Position;
+    var ch         := pr.str.ReadString(1)[1];
+
+        if (ch in Digits) then begin
+           result.ok := true;
+           result.s  := pr.s+ch;
+        end;
+        result.str.Position := same;
+  end;
+
+  function Parse_Comma(pr:TParseResult):TParseResult;
+  begin
+        result.ok  := false;
+        result.s   := pr.s;
+        result.str := pr.str;
+    var same       := pr.str.Position;
+    var ch         := pr.str.ReadString(1)[1];
+
+        if (ch = ',') then begin
+           result.ok := true;
+           result.s  := pr.s+ch;
+        end;
+        result.str.Position := same;
+  end;
+
+  function Parse_EndParen(pr:TParseResult):TParseResult;
+  begin
+        result.ok  := false;
+        result.s   := pr.s;
+        result.str := pr.str;
+    var same       := pr.str.Position;
+    var ch         := pr.str.ReadString(1)[1];
+
+        if (ch = ')') then begin
+           result.ok := true;
+           result.s  := pr.s;
+        end;
+        result.str.Position := same;
+  end;
+
+
+
+
+end.
